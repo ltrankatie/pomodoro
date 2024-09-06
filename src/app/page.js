2616cf227fca033ab5eb03543167cf71396dc1a2
Katie Tran <katietran@lawn-143-215-122-112.lawn.gatech.edu>
2024-09-05 21:38:44 -0400

    25 minutes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
 import Timer from "./ui/timer";
+import { useState } from 'react';
+
+const SECONDS_PER_MIN = 60;
+const POMODORO_TIME_SECS = 25 * SECONDS_PER_MIN;
 
 export default function Home() {
+  const [timeLeft, setTimeLeft] = useState(POMODORO_TIME_SECS);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-      <Timer timeLeft={90} />
+      <Timer timeLeft={timeLeft} />
       </div>
     </main>
   );
