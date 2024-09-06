47b20e7d6b8f97ce55c9427e607518100546e868
Katie Tran <ktran337@gatech.edu>
2024-09-05 21:47:15 -0400

    Countdown

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Timer from "./ui/timer";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const SECONDS_PER_MIN = 60;
 const POMODORO_TIME_SECS = 25 * SECONDS_PER_MIN;
 
 export default function Home() {
   const [timeLeft, setTimeLeft] = useState(POMODORO_TIME_SECS);
+  function countDown() {
+    if (timeLeft > 0) {
+      setTimeLeft(timeLeft - 1);
+    }
+  }
+  useEffect(() => {
+    const interval = setInterval(countDown, 1000 /* ms */);
+    return () => clearInterval(interval);
+  }, [
+    timeLeft,
+  ]);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
